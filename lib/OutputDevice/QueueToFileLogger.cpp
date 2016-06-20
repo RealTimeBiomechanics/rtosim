6c239ce4d2ec2a8ff7276932d2989316ce1a267e
Claudio Pizzolato <claudio.pizzolato@griffithuni.edu.au>
2016-06-20 14:06:20 +1000

    Add license headers to files

--- lib/OutputDevice/QueueToFileLogger.cpp
+++ lib/OutputDevice/QueueToFileLogger.cpp
@@ -1,30 +1,17 @@
 /* -------------------------------------------------------------------------- *
-* CEINMS is a standalone toolbox for neuromusculoskeletal modelling and      *
-* simulation. CEINMS can also be used as a plugin for OpenSim either         *
-* through the OpenSim GUI or API. See https://simtk.org/home/ceinms and the  *
-* NOTICE file for more information. CEINMS development was coordinated       *
-* through Griffith University and supported by the Australian National       *
-* Health and Medical Research Council (NHMRC), the US National Institutes of *
-* Health (NIH), and the European Union Framework Programme 7 (EU FP7). Also  *
-* see the PROJECTS file for more information about the funding projects.     *
-*                                                                            *
-* Copyright (c) 2010-2015 Griffith University and the Contributors           *
-*                                                                            *
-* CEINMS Contributors: C. Pizzolato, M. Reggiani, M. Sartori,                *
-*                      E. Ceseracciu, and D.G. Lloyd                         *
-*                                                                            *
-* Author(s): E. Ceseracciu, C. Pizzolato, M. Reggiani                        *
-*                                                                            *
-* CEINMS is licensed under the Apache License, Version 2.0 (the "License").  *
-* You may not use this file except in compliance with the License. You may   *
-* obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.*
-*                                                                            *
-* Unless required by applicable law or agreed to in writing, software        *
-* distributed under the License is distributed on an "AS IS" BASIS,          *
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
-* See the License for the specific language governing permissions and        *
-* limitations under the License.                                             *
-* -------------------------------------------------------------------------- */
+ * Copyright (c) 2010-2016 C. Pizzolato, M. Reggiani                          *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at:                                   *
+ * http://www.apache.org/licenses/LICENSE-2.0                                 *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ * -------------------------------------------------------------------------- */
 
 #include "rtosim/EndOfData.h"
 #include <iostream>
--- lib/Utilities/OsimUtilities.cpp
+++ lib/Utilities/OsimUtilities.cpp
@@ -1,25 +1,23 @@
+/* -------------------------------------------------------------------------- *
+ * Copyright (c) 2010-2016 C. Pizzolato, M. Reggiani                          *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at:                                   *
+ * http://www.apache.org/licenses/LICENSE-2.0                                 *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ * -------------------------------------------------------------------------- */
+
 #include "rtosim/OsimUtilities.h"
 #include "rtosim/ArrayConverter.h"
 
 namespace rtosim {
 
-    //you may want to use this to avoid getting the coordinate from a modified model
-    /* std::vector<std::string> getCoordinateNamesFromModel(std::string modelFilename) {
-
-         OpenSim::Model model(modelFilename);
-         auto coordNames(getCoordinateNamesFromModel(model));
-         return coordNames;
-         }
-         */
-
-    /*
-    std::vector<std::string> getMarkerNamesFromModel(std::string modelFilename) {
-
-    OpenSim::Model model(modelFilename);
-    auto markerNames(getMarkerNamesFromModel(model));
-    return markerNames;
-    }
-    */
     std::vector<std::string> getMarkerNamesFromModel(const std::string& modelFilename) {
 
         OpenSim::Model model(modelFilename);
